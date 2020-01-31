f0a34485b88b3db79e87b8d72a090baeedc4e1b7
HoriThe3rd <hori-sansei@edu.brain.kyutech.ac.jp>
2020-01-31 09:03:25 +0900

    Bug fix. Change the endian: big to little.

--- python/xillybus.py
+++ python/xillybus.py
@@ -7,12 +7,12 @@ class Xillybus32:
         self.fd_r = os.open(dev_path_r, os.O_RDONLY)
 
     def write(self, data):
-        bytes_w = data.to_bytes(4, 'big')
+        bytes_w = data.to_bytes(4, 'little')
         os.write(self.fd_w, bytes_w)
 
     def read(self):
         byte_r = os.read(self.fd_r, 4)
-        return int.from_bytes(byte_r, 'big')
+        return int.from_bytes(byte_r, 'little')
 
 class Xillybus8:
     def __init__(self, dev_path_w='/dev/xillybus_write_8', dev_path_r='/dev/xillybus_read_8'):
@@ -20,9 +20,9 @@ class Xillybus8:
         self.fd_r = os.open(dev_path_r, os.O_RDONLY)
 
     def write(self, data):
-        bytes_w = data.to_bytes(1, 'big')
+        bytes_w = data.to_bytes(1, 'little')
         os.write(self.fd_w, bytes_w)
 
     def read(self):
         byte_r = os.read(self.fd_r, 1)
-        return int.from_bytes(byte_r, 'big')
+        return int.from_bytes(byte_r, 'little')
