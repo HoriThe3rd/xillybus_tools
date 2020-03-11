be8c24a88af69ae347853c2124a670e02ec9be8e
HoriThe3rd <hori-sansei@edu.brain.kyutech.ac.jp>
2020-03-11 18:16:03 +0900

    Support numpy arrays.

--- python/xillybus.py
+++ python/xillybus.py
@@ -1,5 +1,7 @@
 # Xillybusでデータをやり取りするためのPython関数たち
 import os
+import numpy as np
+
 
 class Xillybus32:
     def __init__(self, dev_path_w='/dev/xillybus_write_32', dev_path_r='/dev/xillybus_read_32'):
@@ -17,6 +19,15 @@ class Xillybus32:
     def read(self):
         byte_r = os.read(self.fd_r, 4)
         return int.from_bytes(byte_r, 'little')
+
+    def write_ndarray(self, data):
+        bytes_w = data.tobytes('C')
+        os.write(self.fd_w, bytes_w)
+
+    def read_ndarray(self, size):
+        bytes_r = os.read(self.fd_r, size)
+        return np.frombuffer(bytes_r, dtype='i4')
+
 
 class Xillybus8:
     def __init__(self, dev_path_w='/dev/xillybus_write_8', dev_path_r='/dev/xillybus_read_8'):
